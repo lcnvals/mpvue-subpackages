1a85fe294a57133323cd604d58e74921f84989a7
isaaczhao <isaaczhao@meijiabang.cn>
2023-01-06 16:13:03 +0800

    init

---
+++ src/packages/order/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+
+    <div>order模块首页</div>
+
+
+    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
